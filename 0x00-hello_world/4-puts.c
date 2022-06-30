532bf1fc1004e0a6c87a5ebaba222f7773d731d3
oluwatoyin <oluwatoyinadediran3@gmail.com>
2022-06-30 16:16:27 -0700

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-/*main -entry point
-  Return: O after printing the function 
- */
-int main (void)
+
+/
+
+ * main - Entry point
+ *
+ *  * Return: Always 0 (sucess)
+ *
+ *   */
+
+ int main(void)
+
 {
-	 put("\"Programming is like building a multilingual puzzle");
-	return 0;
+
+	puts( "Programming is like building a multilingual puzzle" );
+
+	return (0);
+
 }
