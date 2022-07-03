70c95ea887573f9500c21fa4ddc4f01244013fcb
oluwatoyin <oluwatoyinadediran3@gmail.com>
2022-07-03 14:45:31 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: Always return 0
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z' ; a++)
+putchar(a);
+putchar("\n");
+return (0);
+}
