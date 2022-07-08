517395926069f1c8ca9a8f4bbda2886d7655ae6a
oluwatoyin <oluwatoyinadediran3@gmail.com>
2022-07-08 11:30:08 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_diagonal -> printing line
+* @n: integer parameter
+*/
+void print_diagonal(int n)
+{
+int n;
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (x = 0; x < n; x++)
+_putchar('\');
+_putchar('\n');
+}
