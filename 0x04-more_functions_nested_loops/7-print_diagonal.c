110188082cb6e8de7cf6dfcab15fa3399ceedcc7
oluwatoyin <oluwatoyinadediran3@gmail.com>
2022-07-08 12:08:58 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,8 +5,7 @@
 */
 void print_diagonal(int n)
 {
-int n;
-int i;
+int x, i;
 if (n <= 0)
 _putchar('\n');
 else
@@ -14,7 +13,9 @@ else
 for (x = 0; x < n; x++)
 {
 for (i = 0; i <= x; i++)
+_putchar(' ');
 _putchar('92');
 _putchar('\n');
-i}
+}
+}
 }
