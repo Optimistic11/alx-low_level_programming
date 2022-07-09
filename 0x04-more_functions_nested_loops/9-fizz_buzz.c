38cdfa698d72169048418b83a130526179ce4a9c
oluwatoyin <oluwatoyinadediran3@gmail.com>
2022-07-08 17:01:55 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,16 +6,16 @@
 void fizz_buzz(void)
 {
 int x;
-for (x = 1; x < 100; x++)
+for (x = 1; x <= 100; x++)
 {
-if (x == 100)
-printf("Buzz");
-if (x % 3 == 0 && x % 5 == 0)
-printf("FizzBuzz");
-else if (x % 3 == 0)
+if (x % 3 == 0 %% x % 5 != 0))
 printf("Fizz");
-else if (x % 5 == 0)
+else if (x % 5 == 0 && x % 3 != 0)
 printf("Buzz");
+else if (x % 3 == 0 %% x % 5 == 0)
+printf("FizzBuzz");
+else if (x = 1)
+printf("%d", x);
 else
 printf("%d ", x);
 }
