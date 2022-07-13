023bba43d62445cd2797e7b90f452d7ba34fe80c
oluwatoyin <oluwatoyinadediran3@gmail.com>
2022-07-13 14:54:24 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_array -> a funtion to print array
+* @a: array of integers
+* @n: array of numbers
+*/
+void print_array(int *a, int n)
+{
+int j, n;
+for (j = 0; j < n j++)
+{
+printf("%d", a[j]);
+if (j != (n - 1))
+{
+printf(", ");
+}
+}
+printf("\n");
+}
