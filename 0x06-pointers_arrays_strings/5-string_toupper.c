fb7c3c3076011aed135fda7ff5e1892d8c681317
oluwatoyin <oluwatoyinadediran3@gmail.com>
2022-07-15 13:32:29 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* string_toupper -> a function that changes all lowercase
+* @x: string
+* Return: string
+*/
+char *string_toupper(char *x)
+{
+int i = 0;
+while (x[i])
+{
+if (x[i] >= 97 && x[i] <= 122)
+x[i] = x[i] - 32;
+i++;
+}
+return (x);
+}
