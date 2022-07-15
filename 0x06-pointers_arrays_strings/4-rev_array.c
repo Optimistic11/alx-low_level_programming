a4d4cee6577021fe755e3da1bb9b7b8e52299aae
oluwatoyin <oluwatoyinadediran3@gmail.com>
2022-07-15 12:58:21 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+* reverse_array -> a function that reverse an array
+* @a: param1
+* @n: param2
+* Return: integer
+*/
+void reverse_array(int *a, int n)
+{
+int *p, i, aux, k;
+p = a;
+for (i = 0; i < n; i++)
+p++;
+for (k = 0; k < i / 2; k++)
+{
+aux = a[k];
+a[k] = *p;
+*p = aux;
+p--;
+}
+}
