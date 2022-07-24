7ebbc2a110ae1a0858069ac049b5d7f2daa1f156
oluwatoyin <oluwatoyinadediran3@gmail.com>
2022-07-24 07:31:40 -0700

    isupper

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
-* _isupper -> this is a function to check upper case alphabet
-* @c : is an integer parameter
-* Return : success
+* _isupper -> this is a function to check upper case alphas
+* @c: an integer paramer
+* Return: something but success
 */
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-return (1);
-return (0);
+return (c >= 'A' && c <= 'Z');
 }
