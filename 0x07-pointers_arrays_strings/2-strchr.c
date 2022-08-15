0ec25f07a4d6a3d23b2cbdf5badf6de9941dea95
oluwatoyin <oluwatoyinadediran3@gmail.com>
2022-08-15 03:45:16 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,21 +3,22 @@
 * _strchr -> string character
 * @s: string given
 * @c: another char
-* @c: another char
+* * Return: *S
 */
 char *_strchr(char *s, char c)
 {
-int a;
-while (1)
+while (*s != '\0')
 {
-a = *s++;
-if (a == c)
+if (*s == c)
 {
-return (s - 1);
+return (s);
 }
-if (a == 0)
+s++;
+}
+if (*s == c)
 {
-return (NULL);
+return (s);
 }
+return (0);
 }
-}
+
