70c7469b571d5abb5e467fe20605c6eaa5768969
oluwatoyin <oluwatoyinadediran3@gmail.com>
2022-08-15 04:15:33 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,24 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * _strspn - search a string for a set of bytes
 * @s: source string
 * @accept: accepted string
-*
 * Return: number of bytes in the init segment
 */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int a = 0, b, t = 0;
-while (accept[a])
+int i;
+while (*s)
 {
-b = 0;
-while (s[b] != 32)
+for (i = 0; accept[i]; i++)
 {
-if (accept[a] == s[b])
+if (*s == accept[i])
 {
-t++;										b++;
+return (s);
 }
-a++;
 }
-return (t);
+s++;
+return (NULL);
 }
